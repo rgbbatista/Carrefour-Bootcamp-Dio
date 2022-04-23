259daa996a7166e8109bedcf502d2e8a2fe63708
rgbbatista <batistagomesrodrigo.rgb@gmail.com>
2022-04-23 09:54:14 -0300

    atualização aula HTML

---
+++ bootcampCarrefour/HTML-CSS3/desevWebPHP/index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Meu Primeiro Site em PHP! Woohoo!</title>
+    <body>
+      <?php
+      for ( $i =0 ; $i < 10 ; $i++){
+        print(" Linha Número " . $i . "<br/>" );
+      }
+      ?>
+    </body>
+  </head>
---
+++ bootcampCarrefour/HTML-CSS3/desevWebPHP/index2.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Meu Primeiro Site em 
+      ! Woohoo!</title>
+    <body>
+      <?
+      
+      for ( $i =0 ; $i < 10 ; $i++){
+        print(" Linha Número " . $i . "<br/ >" );
+      }
+      ?>
+    </body>
+  </head>
